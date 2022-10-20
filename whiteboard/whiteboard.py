ead2b25f1054e9bb3524a36a0337bc2870bbabf1
ashan <indrajee79@gmail.com>
2022-10-20 21:52:40 +0530

    gui

--- whiteboard/whiteboard.py
+++ whiteboard/whiteboard.py
@@ -10,11 +10,33 @@ root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
 #icon
-image_icon= PhotoImage(file="")
+image_icon= PhotoImage(file="logo.png")
 root.iconphoto(False,image_icon)
 
-color_box=PhotoImage(file="")
+color_box=PhotoImage(file="color section.png")
 Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
+eraser=PhotoImage(file="eraser.png")
+Button(root, image=eraser, bg="#f2f3f5").place(x=30,y=400)
+
+colors=Canvas(root,bg="#ffffff",width=37,height=300,bd=0)
+colors.place(x=30,y=60)
+
+def display_pallete():
+    id = colors.create_rectangle((10,10,30,30),fill='black')
+    colors.tag_bind(id, 'Button-1', lambda x: show_color('black'))
+display_pallete()
+
+canvas= Canvas(root,width=930,height=500,background="#fff",cursor="hand2")
+canvas.place(x=100,y=10)
+
+canvas.bind('<Button-1>')
+canvas.bind('<B1-Motion>')
+
+
+
+
+
 
 
 root.mainloop()
