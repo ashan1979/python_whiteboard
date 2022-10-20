5524a88199852476a1a382ce5a4f0ffe71255504
ashan <indrajee79@gmail.com>
2022-10-20 16:21:32 +0530

    more code

--- whiteboard/whiteboard.py
+++ whiteboard/whiteboard.py
@@ -10,4 +10,11 @@ root.configure(bg="#f2f3f5")
 root.resizable(False,False)
 
 #icon
+image_icon= PhotoImage(file="")
+root.iconphoto(False,image_icon)
+
+color_box=PhotoImage(file="")
+Label(root,image=color_box,bg="#f2f3f5").place(x=10,y=20)
+
+
 root.mainloop()
