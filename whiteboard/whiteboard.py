63d5f081a6db9160c672569ba60d166531a11e77
ashan <indrajee79@gmail.com>
2022-10-20 15:37:10 +0530

    gui

--- whiteboard/whiteboard.py
+++ whiteboard/whiteboard.py
@@ -3,8 +3,11 @@ from tkinter.colorchooser import askcolor
 from tkinter import ttk
 import tkinter as tk
 
-root = tk()
+root = Tk()
 root.title("White Board")
 root.geometry("1050x570+150+50")
 root.configure(bg="#f2f3f5")
 root.resizable(False,False)
+
+#icon
+root.mainloop()
