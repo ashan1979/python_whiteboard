d6450fc82ac44cbaa60c2621e557da21e5f787ef
ashan <indrajee79@gmail.com>
2022-10-20 14:11:18 +0530

    initializing

--- whiteboard.py
+++ whiteboard.py
@@ -4,4 +4,7 @@ from tkinter import ttk
 import tkinter as tk
 
 root = tk()
-root.title()
+root.title("White Board")
+root.geometry("1050x570+150+50")
+root.configure(bg="#f2f3f5")
+root.resizable(False,False)
